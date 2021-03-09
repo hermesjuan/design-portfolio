3e3078379287d62d866231d6d625c4687840e182
Hermes <hermesgonzales@outlook.com>
2021-03-09 16:06:17 -0300

    metadata changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Its Hermes!`,
+    title: `Hello!`,
     description: `I'm looking for the next step in my career. Take a look at some of the stuff i did so far`,
     keyboards: 'design, remote, UI/UX, portfolio, hiring, visual designer, Hermes Juan',
     author: `@hermesjuan`,
